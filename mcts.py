283f3338e4507e42d30c6e5b59f764abbc3d3a47
Kaito Funatsuki <e185732@ie.u-ryukyu.ac.jp>
2021-01-08 23:09:18 +0900

    モンテカルロのファイルを追加

---
+++ mcts.py
@@ -0,0 +1,55 @@
+import math
+import random
+import Board
+import copy
+import treenode
+import pickle
+import state
+from In_Out_put import In_Out_put
+
+class MCTS:
+    budget = 20
+    def __init__(self,root_node):
+        self.reader = In_Out_put()
+        self.L = self.reader.LENGTH
+        self.root_node = root_node
+        self.current_node = None
+        self.history = []
+
+    def find(self,target_node):
+        return self.root_node.find(target_node)
+
+    def serialize(self,root):
+        root.total_visits_for_pickling = treenode.Node.total_visits
+        with open('data.pickle', 'wb') as f:
+           pickle.dump(root, f, pickle.HIGHEST_PROTOCOL)
+
+    def start(self,target_node,serialization=False):
+        if target_node is None:
+            s = state.State(Board.Board(self.L))
+            target_node = treenode.Node(s, None)
+
+        base_node = self.find(target_node)
+
+        for i in range(MCTS.budget):
+            node = base_node
+            if i % 1000 == 0 :
+                print("searching in " + str(i) + " iteration")
+            while not node.is_end():
+                if node.is_fully_expanded():
+                    node = node.select()
+                    if(node is None) :
+                        print("NONE")
+                else:
+                    node = node.expand()
+                    break
+
+            p = node.simulate()
+            node.back_propagate(p)
+
+        if serialization:
+            self.serialize(base_node)
+
+
+
+
