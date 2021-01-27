0a2b207974534cf2ea5d2fb2c94059b2d897b689
hnaga84 <sfbnv17@gmail.com>
2021-01-27 22:49:16 +0900

    改行

--- Util.py
+++ Util.py
@@ -9,11 +9,13 @@ def get_opponent(color):
     else:
         return WALL
 
+
 def get_flatten(unflatten_point, l):
     a = unflatten_point[0] * l*l
     b = a + unflatten_point[1] * l
     ret = b + unflatten_point[2]
     return ret
+
 
 def get_unflatten(flatten_point, l):
     z = flatten_point // (l*l)
