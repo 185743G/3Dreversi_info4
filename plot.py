d8fbadbb54f11c328f62c476eb0de5f523829d18
hnaga84 <sfbnv17@gmail.com>
2021-01-31 00:48:03 +0900

    前後の数100試合の平均をプロットするように変更, 日本語の軸ラベルを表示するように変更

--- plot.py
+++ plot.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
+# from matplotlib.font_manager import FontProperties
+# fp = FontProperties(fname=r'/Library/Fonts/Osaka.ttf', size=16)
+import japanize_matplotlib
+
+
+
 white_tar = "Whiteの勝利数: "
 black_tar = "Blackの勝利数: "
 white_lis = []
@@ -15,6 +21,10 @@ while  True:
 white_sum = 0
 black_sum = 0
 step = 100
+
+def calc_ave(lis):
+	return sum(lis)/len(lis)
+
 
 white_steps = []
 black_steps = []
@@ -33,15 +43,20 @@ for i in range(len(white_lis)):
 	white_sum = w
 	black_sum = b
 	
-win_rate = [white_steps[i] /100 for i in range(len(white_steps))]
+win_rate = [white_steps[i]/step for i in range(len(white_steps))]
+mask = 10
+print(win_rate)
+win_rate = [calc_ave(win_rate[i:i+mask]) for i in range(0, len(white_steps), mask)]
 
 # print(white_steps)
 # print(black_steps)
 # 
 # print(win_rate)
-x = [i*step for i in range(len(win_rate))]
+x = [i*step*mask for i in range(len(win_rate))]
 y = win_rate
 fig = plt.figure()
+plt.xlabel('試合数')
+plt.ylabel('勝率')
 plt.plot(x, y)
 
 print(win_rate)
